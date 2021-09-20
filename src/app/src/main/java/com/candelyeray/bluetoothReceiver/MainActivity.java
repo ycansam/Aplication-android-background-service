525355ac4bc5f962e870e9d313ca4fa4acfcd0ad
Yeray Candel <ycansam@epsg.upv.es>
2021-09-20 19:52:00 +0200

    Aplicacion Unificada
    El receptor de beacons funciona

--- src/app/src/main/java/com/candelyeray/bluetoothReceiver/MainActivity.java
+++ src/app/src/main/java/com/candelyeray/bluetoothReceiver/MainActivity.java
@@ -1,17 +1,331 @@
 package com.candelyeray.bluetoothReceiver;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    private static final String ETIQUETA_LOG = ">>>>";
+
+    // SERVICIOS -------------
+    private Intent elIntentDelServicio = null;
+
+    // BTLE ------------------
+    private BluetoothAdapter.LeScanCallback  callbackLeScan = null;
+    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
+    private static final int PERMISSION_REQUEST_BACKGROUND_LOCATION = 2;
+
+    // PETICIONARIO REST -----
+    private TextView elTexto;
+    private Button elBotonEnviar;
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    public void botonArrancarServicioPulsado( View v ) {
+        Log.d(ETIQUETA_LOG, " boton arrancar servicio Pulsado" );
+
+        if ( this.elIntentDelServicio != null ) {
+            // ya estaba arrancado
+            return;
+        }
+
+        Log.d(ETIQUETA_LOG, " MainActivity.constructor : voy a arrancar el servicio");
+
+        this.elIntentDelServicio = new Intent(this, ServicioEscucharBeacons.class);
+
+        this.elIntentDelServicio.putExtra("tiempoDeEspera", (long) 5000);
+        startService( this.elIntentDelServicio );
+
+    } // ()
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    public void botonDetenerServicioPulsado( View v ) {
+
+        if ( this.elIntentDelServicio == null ) {
+            // no estaba arrancado
+            return;
+        }
+
+        stopService( this.elIntentDelServicio );
+
+        this.elIntentDelServicio = null;
+
+        Log.d(ETIQUETA_LOG, " boton detener servicio Pulsado" );
+
+
+    } // ()
+    // --------------------------------------------------------------
+    // FUNCIONES BTLE
+    // --------------------------------------------------------------
+
+
+    private void buscarTodosLosDispositivosBTLE() {
+        this.callbackLeScan = new BluetoothAdapter.LeScanCallback() {
+            @Override
+            public void onLeScan(BluetoothDevice bluetoothDevice, int rssi, byte[] bytes) {
+
+                //
+                //  se ha encontrado un dispositivo
+                //
+                mostrarInformacionDispositivoBTLE( bluetoothDevice, rssi, bytes );
+
+            } // onLeScan()
+        }; // new LeScanCallback
+        //
+        //
+        boolean resultado = BluetoothAdapter.getDefaultAdapter().startLeScan( this.callbackLeScan );
+
+        // Log.d(ETIQUETA_LOG, " buscarTodosLosDispositivosBTL(): startLeScan(), resultado= " + resultado );
+    } // ()
+
+    // --------------------------------------------------------------
+    // --------------------------------------------------------------
+    private void mostrarInformacionDispositivoBTLE( BluetoothDevice bluetoothDevice, int rssi, byte[] bytes) {
+
+        Log.d(ETIQUETA_LOG, " ****************************************************");
+        Log.d(ETIQUETA_LOG, " ****** DISPOSITIVO DETECTADO BTLE ****************** ");
+        Log.d(ETIQUETA_LOG, " ****************************************************");
+        Log.d(ETIQUETA_LOG, " nombre = " + bluetoothDevice.getName());
+        Log.d(ETIQUETA_LOG, " dirección = " + bluetoothDevice.getAddress());
+        Log.d(ETIQUETA_LOG, " rssi = " + rssi );
+
+        Log.d(ETIQUETA_LOG, " bytes = " + new String(bytes));
+        Log.d(ETIQUETA_LOG, " bytes (" + bytes.length + ") = " + Utilidades.bytesToHexString(bytes));
+
+        TramaIBeacon tib = new TramaIBeacon(bytes);
+
+        Log.d(ETIQUETA_LOG, " ----------------------------------------------------");
+        Log.d(ETIQUETA_LOG, " prefijo  = " + Utilidades.bytesToHexString(tib.getPrefijo()));
+        Log.d(ETIQUETA_LOG, "          advFlags = " + Utilidades.bytesToHexString(tib.getAdvFlags()));
+        Log.d(ETIQUETA_LOG, "          advHeader = " + Utilidades.bytesToHexString(tib.getAdvHeader()));
+        Log.d(ETIQUETA_LOG, "          companyID = " + Utilidades.bytesToHexString(tib.getCompanyID()));
+        Log.d(ETIQUETA_LOG, "          iBeacon type = " + Integer.toHexString(tib.getiBeaconType()));
+        Log.d(ETIQUETA_LOG, "          iBeacon length 0x = " + Integer.toHexString(tib.getiBeaconLength()) + " ( "
+                + tib.getiBeaconLength() + " ) ");
+        Log.d(ETIQUETA_LOG, " uuid  = " + Utilidades.bytesToHexString(tib.getUUID()));
+        Log.d(ETIQUETA_LOG, " uuid  = " + Utilidades.bytesToString(tib.getUUID()));
+        Log.d(ETIQUETA_LOG, " major  = " + Utilidades.bytesToHexString(tib.getMajor()) + "( "
+                + Utilidades.bytesToInt(tib.getMajor()) + " ) ");
+        Log.d(ETIQUETA_LOG, " minor  = " + Utilidades.bytesToHexString(tib.getMinor()) + "( "
+                + Utilidades.bytesToInt(tib.getMinor()) + " ) ");
+        Log.d(ETIQUETA_LOG, " txPower  = " + Integer.toHexString(tib.getTxPower()) + " ( " + tib.getTxPower() + " )");
+        Log.d(ETIQUETA_LOG, " ****************************************************");
+
+    } // ()
+    // --------------------------------------------------------------
+    // --------------------------------------------------------------
+    private void buscarEsteDispositivoBTLE(final UUID dispositivoBuscado ) {
+        this.callbackLeScan = new BluetoothAdapter.LeScanCallback() {
+            @Override
+            public void onLeScan(BluetoothDevice bluetoothDevice, int rssi, byte[] bytes) {
+
+                //
+                // dispostivo encontrado
+                //
+
+                TramaIBeacon tib = new TramaIBeacon( bytes );
+                String uuidString =  Utilidades.bytesToString( tib.getUUID() );
+
+                if ( uuidString.compareTo( Utilidades.uuidToString( dispositivoBuscado ) ) == 0 )  {
+                    detenerBusquedaDispositivosBTLE();
+                    mostrarInformacionDispositivoBTLE( bluetoothDevice, rssi, bytes );
+                } else {
+                    Log.d( MainActivity.ETIQUETA_LOG, " * UUID buscado >" +
+                            Utilidades.uuidToString( dispositivoBuscado ) + "< no concuerda con este uuid = >" + uuidString + "<");
+                }
+
+            } // onLeScan()
+        }; // new LeScanCallback
+
+        //
+        //
+        //
+        BluetoothAdapter.getDefaultAdapter().startLeScan( this.callbackLeScan );
+    } // ()
+    // --------------------------------------------------------------
+    // --------------------------------------------------------------
+    private void detenerBusquedaDispositivosBTLE() {
+        if ( this.callbackLeScan == null ) {
+            return;
+        }
+
+        //
+        //
+        //
+        BluetoothAdapter.getDefaultAdapter().stopLeScan(this.callbackLeScan);
+        this.callbackLeScan = null;
+    } // ()
+
+    // --------------------------------------------------------------
+    // --------------------------------------------------------------
+    public void botonBuscarDispositivosBTLEPulsado( View v ) {
+        Log.d(ETIQUETA_LOG, " boton buscar dispositivos BTLE Pulsado" );
+        this.buscarTodosLosDispositivosBTLE();
+    } // ()
+
+    // --------------------------------------------------------------
+    // --------------------------------------------------------------
+    public void botonBuscarNuestroDispositivoBTLEPulsado( View v ) {
+        Log.d(ETIQUETA_LOG, " boton nuestro dispositivo BTLE Pulsado" );
+        this.buscarEsteDispositivoBTLE( Utilidades.stringToUUID( "EPSG-GTI-PROY-3A" ) );
+    } // ()
+
+    // --------------------------------------------------------------
+    // --------------------------------------------------------------
+    public void botonDetenerBusquedaDispositivosBTLEPulsado( View v ) {
+        Log.d(ETIQUETA_LOG, " boton nuestro dispositivo BTLE Pulsado" );
+        this.detenerBusquedaDispositivosBTLE();
+    } // ()
+
+    // Funciones Peticionario Rest
+
+    //-------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
+    public void boton_enviar_pulsado (View quien) {
+        Log.d("clienterestandroid", "boton_enviar_pulsado");
+        this.elTexto.setText("pulsado");
+
+        // ojo: creo que hay que crear uno nuevo cada vez
+        PeticionarioREST elPeticionario = new PeticionarioREST();
+
+		/*
+
+		   enviarPeticion( "hola", function (res) {
+		   		res
+		   })
+
+        elPeticionario.hacerPeticionREST("GET",  "http://158.42.144.126:8080/prueba", null,
+			(int codigo, String cuerpo) => { } );
+
+		   */
+
+        elPeticionario.hacerPeticionREST("GET",  "http://158.42.144.126:8080/prueba", null,
+                new PeticionarioREST.RespuestaREST () {
+                    @Override
+                    public void callback(int codigo, String cuerpo) {
+                        elTexto.setText ("codigo respuesta= " + codigo + " <-> \n" + cuerpo);
+                    }
+                }
+        );
+
+        // (int codigo, String cuerpo) -> { elTexto.setText ("lo que sea"=; }
+
+        //String textoJSON = "{ 'dni': '" + elDni + "' }";
+       // "{ 'dni': '2023423434' }";
+        /*
+
+
+		/* otro ejemplo:
+		elPeticionario.hacerPeticionREST("POST", "http://192.168.1.113:8080/mensaje",
+				"{\"dni\": \"A9182342W\", \"nombre\": \"Android\", \"apellidos\": \"De Los Palotes\"}",
+				new PeticionarioREST.RespuestaREST () {
+					@Override
+					public void callback(int codigo, String cuerpo) {
+						elTexto.setText ("cdigo respuesta: " + codigo + " <-> \n" + cuerpo);
+					}
+		});
+		*/
+
+		/*
+        elPeticionario.hacerPeticionREST("GET",  "https://jsonplaceholder.typicode.com/posts/2", ...
+        */
+    } // pulsado ()
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(ETIQUETA_LOG, " MainActivity.constructor : empieza");
+        Log.d(ETIQUETA_LOG, " MainActivity.constructor : acaba");
+
+        // Servicio Rest
+        this.elTexto = (TextView) findViewById(R.id.elTexto);
+        this.elBotonEnviar = (Button) findViewById(R.id.botonEnviar);
+
+
+        Log.d("clienterestandroid", "fin onCreate()");
+
+        // Permisos
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                if (this.checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    if (this.shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setTitle("This app needs background location access");
+                        builder.setMessage("Please grant location access so this app can detect beacons in the background.");
+                        builder.setPositiveButton(android.R.string.ok, null);
+                        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                            @TargetApi(23)
+                            @Override
+                            public void onDismiss(DialogInterface dialog) {
+                                requestPermissions(new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                                        PERMISSION_REQUEST_BACKGROUND_LOCATION);
+                            }
+
+                        });
+                        builder.show();
+                    }
+                    else {
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setTitle("Functionality limited");
+                        builder.setMessage("Since background location access has not been granted, this app will not be able to discover beacons in the background.  Please go to Settings -> Applications -> Permissions and grant background location access to this app.");
+                        builder.setPositiveButton(android.R.string.ok, null);
+                        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                            @Override
+                            public void onDismiss(DialogInterface dialog) {
+                            }
+
+                        });
+                        builder.show();
+                    }
+
+                }
+            } else {
+                if (!this.shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                            PERMISSION_REQUEST_FINE_LOCATION);
+                }
+                else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons.  Please go to Settings -> Applications -> Permissions and grant location access to this app.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+
+            }
+        }
     }
 }
