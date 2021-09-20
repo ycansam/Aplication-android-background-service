55355eb5d09e8d5c59434eb5020e5dc7a7aa31ac
Yeray Candel <ycansam@epsg.upv.es>
2021-09-20 17:58:44 +0200

    Creado proyecto

---
+++ src/app/src/main/java/com/candelyeray/bluetoothReceiver/MainActivity.java
@@ -0,0 +1,17 @@
+package com.candelyeray.bluetoothReceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
